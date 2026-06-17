9158cb91f14f60b8890a59f8bf43306a0bbdc11f
gustn99 <azerosong@gmail.com>
2026-06-17 19:18:37 +0900

    chore: jsdom dialog 스펙 추가

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -4,6 +4,33 @@ import { CART_QUERY_KEY } from "@hooks/useCartQuery";
 import { queryStore } from "@/queries/instance";
 import { seedCarts, server } from "@/tests/setup/server";
 
+// JSDOM does not support HTMLDialogElement's showModal and close methods.
+if (typeof HTMLDialogElement === "function") {
+  HTMLDialogElement.prototype.showModal = function () {
+    this.setAttribute("open", "");
+  };
+  HTMLDialogElement.prototype.close = function () {
+    this.removeAttribute("open");
+    this.dispatchEvent(new Event("close"));
+  };
+}
+
+// Polyfill for ESC key closing behavior for <dialog>
+if (typeof window !== "undefined") {
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      const openDialogs = document.querySelectorAll<HTMLDialogElement>("dialog[open]");
+      openDialogs.forEach((dialog) => {
+        const cancelEvent = new Event("cancel", { cancelable: true });
+        dialog.dispatchEvent(cancelEvent);
+        if (!cancelEvent.defaultPrevented) {
+          dialog.close();
+        }
+      });
+    }
+  });
+}
+
 // MSW 수명주기: 처리되지 않은 요청은 오류로 처리해 누락을 빠르게 드러낸다.
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterEach(() => server.resetHandlers());
