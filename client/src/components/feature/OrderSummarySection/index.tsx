5402fdccfd3bd7dc8870c04536f48077ae00b357
gustn99 <azerosong@gmail.com>
2026-06-20 14:48:06 +0900

    refactor: OrderSummaryRow를 컴포넌트로 분리

--- client/src/components/feature/OrderSummarySection/index.tsx
+++ client/src/components/feature/OrderSummarySection/index.tsx
@@ -1,81 +1,45 @@
 import info from "@assets/info.svg";
+import OrderSummaryRow from "@components/common/entities/OrderSummaryRow";
 import Text from "@components/common/shared/Text";
 import Spacing from "@components/common/shared/Spacing";
 import Divider from "@components/common/shared/Divider";
+import Flex from "@components/common/shared/Flex";
 import styled from "@emotion/styled";
 
 export default function OrderSummarySection() {
   // summary 정보 조회
 
   return (
-    <CartOrderAmountContainer data-testid="order-summary">
-      <OrderAmountInfoWrapper>
+    <OrderSummarySectionContainer data-testid="order-summary">
+      <Flex gap={4}>
         <InfoIcon src={info} alt="정보" />
         <Text typograph="caption" as="span">
           총 주문 금액이 100,000원 이상일 경우 무료 배송됩니다.
         </Text>
-      </OrderAmountInfoWrapper>
+      </Flex>
+
       <Spacing size={0.75} />
       <Divider />
       <Spacing size={0.75} />
-      <CartOrderAmountInfoContainer>
-        <Text typograph="body1" as="span">
-          총 주문 금액
-        </Text>
-        <Text typograph="heading1" as="span">
-          {(10000).toLocaleString()}원
-        </Text>
-      </CartOrderAmountInfoContainer>
+
+      <OrderSummaryRow label="총 주문 금액" value={10000} />
       <Spacing size={0.5} />
-      <CartOrderAmountInfoContainer>
-        <Text typograph="body1" as="span">
-          쿠폰 할인 금액
-        </Text>
-        <Text typograph="heading1" as="span">
-          {(-3000).toLocaleString()}원
-        </Text>
-      </CartOrderAmountInfoContainer>
+      <OrderSummaryRow label="쿠폰 할인 금액" value={-3000} />
       <Spacing size={0.5} />
-      <CartOrderAmountInfoContainer>
-        <Text typograph="body1" as="span">
-          배송비
-        </Text>
-        <Text typograph="heading1" as="span">
-          {(3000).toLocaleString()}원
-        </Text>
-      </CartOrderAmountInfoContainer>
+      <OrderSummaryRow label="배송비" value={3000} />
+
       <Spacing size={0.75} />
       <Divider />
       <Spacing size={0.75} />
-      <CartOrderAmountInfoContainer>
-        <Text typograph="body1" as="span">
-          총 결제 금액
-        </Text>
-        <Text typograph="heading1" as="span">
-          {(10000).toLocaleString()}원
-        </Text>
-      </CartOrderAmountInfoContainer>
-    </CartOrderAmountContainer>
+
+      <OrderSummaryRow label="총 결제 금액" value={10000} />
+    </OrderSummarySectionContainer>
   );
 }
 
-const CartOrderAmountContainer = styled.div``;
-
-const OrderAmountInfoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.25rem;
-`;
+const OrderSummarySectionContainer = styled.div``;
 
 const InfoIcon = styled.img`
   width: 0.875rem;
   aspect-ratio: 1/1;
-`;
-
-const CartOrderAmountInfoContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 0.25rem;
-  padding-block: 0.5rem;
 `;
