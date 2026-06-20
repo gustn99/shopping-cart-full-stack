f4d8591fb01a0e2f83e7d4ea0914b826ee4aae10
gustn99 <azerosong@gmail.com>
2026-06-21 00:53:10 +0900

    feat: 공통 버튼 컴포넌트 intent 추가

--- client/src/components/common/shared/Button/index.tsx
+++ client/src/components/common/shared/Button/index.tsx
@@ -6,6 +6,7 @@ interface ButtonStyleProps {
   fullWidth?: boolean;
   rounded?: boolean;
   variant?: "solid" | "outline";
+  intent?: "primary" | "secondary";
   size?: "md" | "lg";
 }
 
@@ -22,13 +23,14 @@ const TEXT_COLOR_MAP = {
 } as const;
 
 const BG_COLOR_MAP = {
-  solid: COLOR_PALETTE.black,
-  outline: COLOR_PALETTE.white,
-} as const;
-
-const BORDER_COLOR_MAP = {
-  solid: COLOR_PALETTE.black,
-  outline: COLOR_PALETTE.border,
+  solid: {
+    primary: COLOR_PALETTE.black,
+    secondary: COLOR_PALETTE["gray-900"],
+  } as const,
+  outline: {
+    primary: COLOR_PALETTE.white,
+    secondary: COLOR_PALETTE.white,
+  } as const,
 } as const;
 
 const PADDING_MAP = {
@@ -39,17 +41,15 @@ const PADDING_MAP = {
 export default function Button({
   fullWidth = false,
   variant = "solid",
+  intent = "primary",
   rounded = false,
   size = "lg",
   children,
   ...props
 }: ButtonProps) {
   return (
-    <ButtonWrapper fullWidth={fullWidth} variant={variant} rounded={rounded} size={size} {...props}>
-      <Text
-        typograph={TYPOGRAPH_MAP[size]}
-        color={TEXT_COLOR_MAP[variant]}
-      >
+    <ButtonWrapper fullWidth={fullWidth} variant={variant} intent={intent} rounded={rounded} size={size} {...props}>
+      <Text typograph={TYPOGRAPH_MAP[size]} color={TEXT_COLOR_MAP[variant]}>
         {children}
       </Text>
     </ButtonWrapper>
@@ -57,14 +57,14 @@ export default function Button({
 }
 
 const ButtonWrapper = styled.button<Required<ButtonStyleProps>>`
-  background-color: ${({ variant }) => BG_COLOR_MAP[variant]};
+  background-color: ${({ variant, intent }) => BG_COLOR_MAP[variant][intent]};
   color: ${COLOR_PALETTE.white};
   font-weight: 700;
   font-size: 1rem;
   line-height: 1rem;
   padding-block: ${({ size }) => PADDING_MAP[size]};
   width: ${({ fullWidth }) => (fullWidth ? "100%" : "auto")};
-  border: 1px solid ${({ variant }) => BORDER_COLOR_MAP[variant]};
+  ${({ variant }) => variant === "outline" && `border: 1px solid  ${COLOR_PALETTE.border}`};
   border-radius: ${({ rounded }) => (rounded ? "5px" : 0)};
 
   :disabled {
