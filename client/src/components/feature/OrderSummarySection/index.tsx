a8e3c096511e588a2fa8f85d36be5ac4ed2b5334
gustn99 <azerosong@gmail.com>
2026-06-22 02:00:17 +0900

    feat: OrderSummarySection에 useOrderQuery 및 useOrderDiscountQuery 적용

--- client/src/components/feature/OrderSummarySection/index.tsx
+++ client/src/components/feature/OrderSummarySection/index.tsx
@@ -5,13 +5,22 @@ import Spacing from "@components/common/shared/Spacing";
 import Divider from "@components/common/shared/Divider";
 import Flex from "@components/common/shared/Flex";
 import styled from "@emotion/styled";
+import useOrderQuery from "@/hooks/useOrderQuery";
+import useOrderDiscountQuery from "@/hooks/useOrderDiscountQuery";
 
 interface OrderSummarySectionProps {
   orderId: number;
 }
 
 export default function OrderSummarySection({ orderId }: OrderSummarySectionProps) {
-  // summary 정보 조회
+  const { data: order } = useOrderQuery(orderId);
+  const { data: discount } = useOrderDiscountQuery(orderId, { couponId: order.coupons });
+
+  const totalOrderAmount = order.products.reduce((acc, p) => acc + p.price * p.quantity, 0);
+  const discountAmount = discount.discountAmount;
+  const deliveryFee = order.deliveryFee ?? 0;
+  
+  const totalPaymentAmount = totalOrderAmount - discountAmount + deliveryFee;
 
   return (
     <OrderSummarySectionContainer data-testid="order-summary">
@@ -26,17 +35,17 @@ export default function OrderSummarySection({ orderId }: OrderSummarySectionProp
       <Divider />
       <Spacing size={0.75} />
 
-      <OrderSummaryRow label="총 주문 금액" value={10000} />
+      <OrderSummaryRow label="총 주문 금액" value={totalOrderAmount} />
       <Spacing size={0.5} />
-      <OrderSummaryRow label="쿠폰 할인 금액" value={-3000} />
+      <OrderSummaryRow label="쿠폰 할인 금액" value={-discountAmount} />
       <Spacing size={0.5} />
-      <OrderSummaryRow label="배송비" value={3000} />
+      <OrderSummaryRow label="배송비" value={deliveryFee} />
 
       <Spacing size={0.75} />
       <Divider />
       <Spacing size={0.75} />
 
-      <OrderSummaryRow label="총 결제 금액" value={10000} />
+      <OrderSummaryRow label="총 결제 금액" value={totalPaymentAmount} />
     </OrderSummarySectionContainer>
   );
 }
---
+++ client/src/hooks/useOrderDiscountQuery.ts
@@ -0,0 +1,14 @@
+import { getDiscount } from "@/apis/orders/[orderId]/discount";
+import useSuspenseQuery from "@/queries/useSuspenseQuery";
+import type { GetDiscountRequest } from "@/types/order";
+
+export const ORDER_DISCOUNT_QUERY_KEY = "order-discount";
+
+export default function useOrderDiscountQuery(orderId: number, params: GetDiscountRequest) {
+  const queryKey = `${ORDER_DISCOUNT_QUERY_KEY}-${orderId}-${params.couponId.join(",")}`;
+
+  return useSuspenseQuery({
+    key: queryKey,
+    queryFn: () => getDiscount(orderId, params),
+  });
+}
