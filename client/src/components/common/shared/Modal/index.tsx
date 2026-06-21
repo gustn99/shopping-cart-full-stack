e70c6afb9b3181c8673c1f1db6aea9cfef7a85a9
gustn99 <azerosong@gmail.com>
2026-06-21 20:46:30 +0900

    feat: Modal 공통 컴포넌트 구현

--- client/src/components/common/shared/Modal/index.tsx
+++ client/src/components/common/shared/Modal/index.tsx
@@ -1,69 +1,52 @@
-import useCheckedItems from "@hooks/useCheckedItems.ts";
-import Button from "@components/common/shared/Button";
+import styled from "@emotion/styled";
+import { COLOR_PALETTE } from "@styles/colorPalette.ts";
+import { useEffect } from "react";
 
 interface ModalProps extends React.ComponentProps<"dialog"> {
-  modalRef: React.RefObject<HTMLDialogElement | null>;
+  shouldLockBackgroundScroll?: boolean;
+  closeOnBackdropClick?: boolean;
+  onClose?: () => void;
 }
 
-export default function Modal({ modalRef, children, ...props }: ModalProps) {
-  const handleClose = () => modalRef.current?.close();
-
+export default function Modal({
+  shouldLockBackgroundScroll,
+  closeOnBackdropClick,
+  onClose,
+  children,
+  ...props
+}: ModalProps) {
   const handleBackdropClick = (e: React.MouseEvent) => {
+    if (!closeOnBackdropClick) return;
+
     if (e.target === e.currentTarget) {
-      handleClose();
+      onClose?.();
     }
   };
 
+  useEffect(() => {
+    if (shouldLockBackgroundScroll) {
+      document.body.style.overflowY = "hidden";
+    }
+
+    return () => {
+      if (shouldLockBackgroundScroll) {
+        document.body.style.overflowY = "";
+      }
+    };
+  }, [shouldLockBackgroundScroll]);
+
   return (
-    <dialog ref={modalRef} onClick={handleBackdropClick} {...props}>
+    <ModalWrapper onClick={handleBackdropClick} {...props}>
       {children}
-    </dialog>
+    </ModalWrapper>
   );
 }
 
-interface CouponModalProps {
-  modalRef: React.RefObject<HTMLDialogElement | null>;
-  onConfirm: (checkedIds: number[]) => void;
-}
+const ModalWrapper = styled.dialog`
+  margin: auto;
+  border: none;
 
-export function CouponModal({ modalRef, onConfirm }: CouponModalProps) {
-  const { checkedItems, select, unselect } = useCheckedItems<number>();
-  const isChecked = (id: number) => checkedItems.includes(id);
-  const canCheckMore = checkedItems.length < 2;
-
-  const handleCouponToggle = (id: number) => {
-    if (isChecked(id)) return unselect(id);
-    select(id);
-  };
-
-  const handleClose = () => modalRef.current?.close();
-
-  const handleConfirm = () => {
-    onConfirm(checkedItems); // id 기반으로 mutation
-  };
-
-  return (
-    <Modal modalRef={modalRef} aria-label="쿠폰">
-      <h2>쿠폰</h2>
-      <button onClick={handleClose} aria-label="닫기">
-        X
-      </button>
-      <ul aria-label="쿠폰 리스트">
-        {[0, 1, 2].map((id, index) => (
-          <li key={id}>
-            <label>
-              <input
-                type="checkbox"
-                disabled={!canCheckMore && !isChecked(id)}
-                checked={isChecked(id)}
-                onChange={() => handleCouponToggle(id)}
-              />
-              쿠폰{index + 1}
-            </label>
-          </li>
-        ))}
-      </ul>
-      <Button onClick={handleConfirm}>쿠폰 사용</Button>
-    </Modal>
-  );
-}
+  ::backdrop {
+    background-color: ${COLOR_PALETTE.dimmed};
+  }
+`;
