64139b8539a0d4c96d11c2dab2b6e2fae09f40aa
gustn99 <azerosong@gmail.com>
2026-06-22 01:03:37 +0900

    test: order-form 페이지 통합 테스트에 msw 결합

--- client/src/tests/orderForm.integration.test.tsx
+++ client/src/tests/orderForm.integration.test.tsx
@@ -1,25 +1,33 @@
 import { screen, within } from "@testing-library/react";
 import { renderCartsApp } from "./setup/renderCartsApp";
 import { ROUTES } from "@constants/routes";
+import { server } from "@/mocks/server";
+import { ordersScenarios } from "@/mocks/scenarios/orders";
 
 // TODO: 대상 요소를 변수로 선언할 건지 인라인으로 넘길 건지 통일
 
+const renderOrderFormApp = () => renderCartsApp({ pathname: ROUTES.ORDER_FORM, state: { orderId: 1 } });
+
 describe("OrderFormPage", () => {
+  beforeEach(() => {
+    server.use(ordersScenarios.getSuccess, ordersScenarios.getCouponsSuccess, ordersScenarios.getDiscountSuccess);
+  });
+
   describe("쿠폰 모달", () => {
     it("쿠폰 적용 버튼 클릭 시 쿠폰 모달이 렌더링된다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
+      const { user } = renderOrderFormApp();
 
-      const couponButton = screen.getByRole("button", { name: /쿠폰 적용/ });
+      const couponButton = await screen.findByRole("button", { name: /쿠폰 적용/ });
       await user.click(couponButton);
 
-      expect(screen.getByRole("dialog", { name: /쿠폰/ })).toBeVisible();
+      expect(await screen.findByRole("dialog", { name: /쿠폰/ })).toBeVisible();
     });
 
     it("쿠폰 아이템 클릭 시 checked 상태가 변경된다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const couponList = screen.getByRole("list", { name: /쿠폰 리스트/ });
+      const couponList = await screen.findByRole("list", { name: /쿠폰 리스트/ });
       const couponCheckbox = within(couponList).getAllByRole("checkbox")[0];
 
       await user.click(couponCheckbox);
@@ -30,13 +38,11 @@ describe("OrderFormPage", () => {
     });
 
     it("쿠폰 아이템이 2개 checked된 경우 다른 checkbox는 disabled된다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const couponList = screen.getByRole("list", { name: /쿠폰 리스트/ });
-      const checkboxes = within(couponList).getAllByRole(
-        "checkbox",
-      ) as HTMLInputElement[];
+      const couponList = await screen.findByRole("list", { name: /쿠폰 리스트/ });
+      const checkboxes = within(couponList).getAllByRole("checkbox") as HTMLInputElement[];
 
       if (checkboxes.length >= 2) {
         await user.click(checkboxes[0]);
@@ -51,42 +57,42 @@ describe("OrderFormPage", () => {
     });
 
     it("쿠폰 사용 버튼 클릭 시 쿠폰 모달이 닫힌다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const dialog = screen.getByRole("dialog", { name: /쿠폰/ });
-      const useButton = screen.getByRole("button", { name: /쿠폰 사용/ });
+      const dialog = await screen.findByRole("dialog", { name: /쿠폰/ });
+      const useButton = within(dialog).getByRole("button", { name: /쿠폰 사용/ });
       await user.click(useButton);
 
       expect(dialog).not.toBeVisible();
     });
 
     it("모달 내 x 버튼 클릭 시 모달이 닫힌다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const dialog = screen.getByRole("dialog", { name: /쿠폰/ });
-      const closeButton = screen.getByRole("button", { name: /닫기/ });
+      const dialog = await screen.findByRole("dialog", { name: /쿠폰/ });
+      const closeButton = within(dialog).getByRole("button", { name: /닫기/ });
       await user.click(closeButton);
 
       expect(dialog).not.toBeVisible();
     });
 
     it("esc 입력 시 모달이 닫힌다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const dialog = screen.getByRole("dialog", { name: /쿠폰/ });
+      const dialog = await screen.findByRole("dialog", { name: /쿠폰/ });
       await user.keyboard("{Escape}");
 
       expect(dialog).not.toBeVisible();
     });
 
     it("모달 외부 클릭 시 모달이 닫힌다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
-      await user.click(screen.getByRole("button", { name: /쿠폰 적용/ }));
+      const { user } = renderOrderFormApp();
+      await user.click(await screen.findByRole("button", { name: /쿠폰 적용/ }));
 
-      const dialog = screen.getByRole("dialog", { name: /쿠폰/ });
+      const dialog = await screen.findByRole("dialog", { name: /쿠폰/ });
       await user.click(dialog);
 
       expect(dialog).not.toBeVisible();
@@ -95,9 +101,9 @@ describe("OrderFormPage", () => {
 
   describe("배송 정보", () => {
     it("도서 산간 지역 클릭 시 checked 상태가 변경된다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
+      const { user } = renderOrderFormApp();
 
-      const checkbox = screen.getByRole("checkbox", {
+      const checkbox = await screen.findByRole("checkbox", {
         name: /도서 산간 지역/,
       });
 
@@ -111,9 +117,10 @@ describe("OrderFormPage", () => {
 
   describe("결제", () => {
     it("결제하기 클릭 시 결제 확인 페이지로 리다이렉트된다", async () => {
-      const { user } = renderCartsApp(ROUTES.ORDER_FORM);
+      server.use(ordersScenarios.postSuccess);
+      const { user } = renderOrderFormApp();
 
-      const payButton = screen.getByRole("button", { name: /결제/ });
+      const payButton = await screen.findByRole("button", { name: /결제/ });
       await user.click(payButton);
 
       expect(await screen.findByText(/결제 확인/)).toBeInTheDocument();
