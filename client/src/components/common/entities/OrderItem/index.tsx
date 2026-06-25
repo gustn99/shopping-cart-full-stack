307571713798125061ac28f02fad8325929976c7
gustn99 <azerosong@gmail.com>
2026-06-25 22:38:25 +0900

    refactor(client): Divider를 OrderItem 밖으로 분리

--- client/src/components/common/entities/OrderItem/index.tsx
+++ client/src/components/common/entities/OrderItem/index.tsx
@@ -1,9 +1,6 @@
 import ProductImg from "@components/common/entities/ProductImg";
-import Divider from "@components/common/shared/Divider";
 import Flex from "@components/common/shared/Flex";
-import Spacing from "@components/common/shared/Spacing";
 import Text from "@components/common/shared/Text";
-import styled from "@emotion/styled";
 import { COLOR_PALETTE } from "@styles/colorPalette.ts";
 
 interface OrderItemProps {
@@ -16,34 +13,28 @@ interface OrderItemProps {
 
 export default function OrderItem({ name, imgUrl, price, quantity, hasGift }: OrderItemProps) {
   return (
-    <OrderItemWrapper>
-      <Divider />
-      <Spacing size={0.75} />
-      <Flex gap={24} align="center">
-        <ProductImg src={imgUrl} alt={name} />
-        <Flex direction="column" gap={24}>
-          <Flex direction="column" gap={4}>
-            <Text typograph="caption" as="p">
-              {name}
+    <Flex gap={24} align="center">
+      <ProductImg src={imgUrl} alt={name} />
+      <Flex direction="column" gap={24}>
+        <Flex direction="column" gap={4}>
+          <Text typograph="caption" as="p">
+            {name}
+          </Text>
+          <Text typograph="heading1" as="p">
+            {price.toLocaleString()}원
+          </Text>
+        </Flex>
+        <Flex gap={8} align="center">
+          <Text typograph="caption" as="span">
+            {quantity}개
+          </Text>
+          {hasGift && (
+            <Text typograph="caption" as="span" color={COLOR_PALETTE.blue}>
+              + 1
             </Text>
-            <Text typograph="heading1" as="p">
-              {price.toLocaleString()}원
-            </Text>
-          </Flex>
-          <Flex gap={8} align="center">
-            <Text typograph="caption" as="span">
-              {quantity}개
-            </Text>
-            {hasGift && (
-              <Text typograph="caption" as="span" color={COLOR_PALETTE.blue}>
-                + 1
-              </Text>
-            )}
-          </Flex>
+          )}
         </Flex>
       </Flex>
-    </OrderItemWrapper>
+    </Flex>
   );
 }
-
-const OrderItemWrapper = styled.li``;
--- client/src/components/feature/ProductListSection/index.tsx
+++ client/src/components/feature/ProductListSection/index.tsx
@@ -1,4 +1,6 @@
+import Divider from "@components/common/shared/Divider";
 import Flex from "@components/common/shared/Flex";
+import Spacing from "@components/common/shared/Spacing";
 import styled from "@emotion/styled";
 import OrderItem from "@components/common/entities/OrderItem";
 import useOrderQuery from "@/hooks/useOrderQuery";
@@ -13,10 +15,16 @@ export default function ProductListSection({ orderId }: ProductListSectionProps)
   return (
     <OrderList as="ul" direction="column" gap={20} aria-label="상품 리스트">
       {order.products.map((item) => (
-        <OrderItem key={item.id} {...item} />
+        <OrderItemWrapper>
+          <Divider />
+          <Spacing size={0.75} />
+          <OrderItem key={item.id} {...item} />
+        </OrderItemWrapper>
       ))}
     </OrderList>
   );
 }
 
 const OrderList = styled(Flex)``;
+
+const OrderItemWrapper = styled.li``;
