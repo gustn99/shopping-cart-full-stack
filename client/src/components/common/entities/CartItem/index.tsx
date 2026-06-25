51bdd0457079ed3c4fad773b8622acb02f8930e5
gustn99 <azerosong@gmail.com>
2026-06-25 23:01:58 +0900

    refactor(client): CartItem 주변 요소를 외부로 분리

--- client/src/components/common/entities/CartItem/index.tsx
+++ client/src/components/common/entities/CartItem/index.tsx
@@ -2,9 +2,6 @@ import minus from "@assets/minus.svg";
 import plus from "@assets/plus.svg";
 import ProductImg from "@components/common/entities/ProductImg";
 import Flex from "@components/common/shared/Flex";
-import Spacing from "@components/common/shared/Spacing";
-import CheckBox from "@components/common/shared/CheckBox";
-import Divider from "@components/common/shared/Divider";
 import Text from "@components/common/shared/Text";
 import styled from "@emotion/styled";
 import { COLOR_PALETTE } from "@styles/colorPalette";
@@ -19,83 +16,44 @@ interface CartItemProps {
   image: string;
   price: number;
   quantity: number;
-  checked: boolean;
   quantityRange: QuantityRange;
-  onSelect: () => void;
-  onDelete: () => void;
   onChangeQuantity: (quantity: number) => void;
 }
 
-export default function CartItem({
-  name,
-  image,
-  price,
-  quantity,
-  checked,
-  onSelect,
-  onDelete,
-  quantityRange,
-  onChangeQuantity,
-}: CartItemProps) {
+export default function CartItem({ name, image, price, quantity, quantityRange, onChangeQuantity }: CartItemProps) {
   return (
-    <CartItemContainer>
-      <Divider />
-      <Spacing size={0.75} />
-      <Flex justify="space-between" align="center">
-        <CheckBox checked={checked} onChange={onSelect} />
-        <DeleteButton onClick={onDelete}>삭제</DeleteButton>
-      </Flex>
-      <Spacing size={0.75} />
-
-      <Flex gap={24} align="center">
-        <ProductImg src={image} alt={name} />
-        <Flex direction="column" gap={24}>
-          <Flex direction="column" gap={4}>
-            <Text typograph="caption" as="p">
-              {name}
+    <Flex gap={24} align="center">
+      <ProductImg src={image} alt={name} />
+      <Flex direction="column" gap={24}>
+        <Flex direction="column" gap={4}>
+          <Text typograph="caption" as="p">
+            {name}
+          </Text>
+          <Text typograph="heading1" as="p">
+            {price.toLocaleString()}원
+          </Text>
+        </Flex>
+        <Flex gap={8} align="center">
+          <QuantityButton
+            src={minus}
+            disabled={quantity <= quantityRange.min}
+            onClick={() => onChangeQuantity(Math.max(1, quantity - 1))}
+          />
+          <Quantity>
+            <Text typograph="caption" as="span">
+              {quantity}
             </Text>
-            <Text typograph="heading1" as="p">
-              {price.toLocaleString()}원
-            </Text>
-          </Flex>
-          <Flex gap={8} align="center">
-            <QuantityButton
-              src={minus}
-              disabled={quantity <= quantityRange.min}
-              onClick={() => onChangeQuantity(Math.max(1, quantity - 1))}
-            />
-            <Quantity>
-              <Text typograph="caption" as="span">
-                {quantity}
-              </Text>
-            </Quantity>
-            <QuantityButton
-              src={plus}
-              disabled={quantity >= quantityRange.max}
-              onClick={() => onChangeQuantity(quantity + 1)}
-            />
-          </Flex>
+          </Quantity>
+          <QuantityButton
+            src={plus}
+            disabled={quantity >= quantityRange.max}
+            onClick={() => onChangeQuantity(quantity + 1)}
+          />
         </Flex>
       </Flex>
-    </CartItemContainer>
+    </Flex>
   );
 }
-
-const CartItemContainer = styled.li``;
-
-const DeleteButton = styled.button`
-  padding: 0.25rem 0.5rem;
-  border-radius: 0.25rem;
-  border: 1px solid ${COLOR_PALETTE.border};
-  background-color: ${COLOR_PALETTE.white};
-  font-weight: 500;
-  font-size: 0.75rem;
-  line-height: 0.9375rem;
-
-  :active {
-    background-color: ${COLOR_PALETTE.border};
-  }
-`;
 
 const QuantityButton = styled.button<{ src: string }>`
   width: 1.5rem;
--- client/src/components/common/entities/CartList/index.tsx
+++ client/src/components/common/entities/CartList/index.tsx
@@ -1,9 +1,12 @@
 import CartItem from "@components/common/entities/CartItem";
 import CheckBox from "@components/common/shared/CheckBox";
+import Divider from "@components/common/shared/Divider";
 import Flex from "@components/common/shared/Flex";
 import Text from "@components/common/shared/Text";
 import type { Cart, Product, QuantityRange } from "@/types/cartProduct";
 import Spacing from "@components/common/shared/Spacing";
+import styled from "@emotion/styled";
+import { COLOR_PALETTE } from "@styles/colorPalette.ts";
 
 interface CartListProps {
   cartProducts: Cart[];
@@ -33,19 +36,41 @@ export default function CartList({
       <Spacing size={1.25} />
       <Flex as="ul" direction="column" gap={20}>
         {cartProducts.map(({ product, quantity }) => (
-          <CartItem
-            key={product.id}
-            {...product}
-            quantity={quantity}
-            checked={checkedItems.includes(product.id)}
-            onSelect={() => onSelect(product.id)}
-            onDelete={() => onDelete(product.id)}
-            quantityRange={quantityRange}
-            onChangeQuantity={(newQuantity) => onChangeQuantity(product.id, newQuantity)}
-          />
+          <CartItemWrapper>
+            <Flex justify="space-between" align="center">
+              <CheckBox checked={checkedItems.includes(product.id)} onSelect={() => onSelect(product.id)} />
+              <DeleteButton onClick={() => onDelete(product.id)}>삭제</DeleteButton>
+            </Flex>
+            <Spacing size={0.75} />
+            <Divider />
+            <Spacing size={0.75} />
+            <CartItem
+              key={product.id}
+              {...product}
+              quantity={quantity}
+              quantityRange={quantityRange}
+              onChangeQuantity={(newQuantity) => onChangeQuantity(product.id, newQuantity)}
+            />
+          </CartItemWrapper>
         ))}
       </Flex>
       <Spacing size={3.25} />
     </Flex>
   );
 }
+
+const CartItemWrapper = styled.li``;
+
+const DeleteButton = styled.button`
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  border: 1px solid ${COLOR_PALETTE.border};
+  background-color: ${COLOR_PALETTE.white};
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 0.9375rem;
+
+  :active {
+    background-color: ${COLOR_PALETTE.border};
+  }
+`;
